10b24c1820665074fbbed496cf380513ca561030
arun varghese <arun26cs@gmail.com>
2020-09-16 12:03:31 -0400

    adding the initial changes,this includes the class component and the person functional component without useState hook

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react'; 
 import './App.css';
+import Person from './Person/Person'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+  state={
+    persons:[
+      {name:'max',age:23},
+      {name:'Arun1',age:34}
+    ],
+    newPerson:'Gooder'
+  };
+
+  nameHandler = () =>{
+    this.setState({
+      persons:[
+        {name:'max!!',age:23},
+        {name:'Arun1',age:34}
+      ],
+      newPerson:'GoodMillain'
+    }); 
+    console.log(this.state.newPerson);/*this is checking if the previous state is also maintained --> yes it is */
+  }
+
+  
+   render(){
+     return(
+       <div className="App">
+         <p>this is a para test from a class component of react</p>
+         <h1>this is the heading component</h1>
+         <button onClick={this.nameHandler}>Click here</button> 
+         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+         <Person name={this.state.newPerson} age="24"/> 
+       </div>
+     );
+   }
 }
 
 export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Person.css';
+
+
+const person = (props) =>{
+    const element = <h1>this is a person function from person component</h1>;
+    const propsElement = <p>my name is {props.name} and my age is {props.age}</p>
+    return (
+        <div className="Person">
+            {element}
+            {propsElement}
+        </div>
+    );
+}
+
+export default person;
