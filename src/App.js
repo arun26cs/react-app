f5d254582d9c0633af489eb71e6bc1c45248f051
arun varghese <arun26cs@gmail.com>
2020-09-16 14:01:40 -0400

    useState hook

--- src/App.js
+++ src/App.js
@@ -1,41 +1,41 @@
-import React,{Component} from 'react'; 
+import React,{Component, useState} from 'react'; 
 import './App.css';
 import Person from './Person/Person'
 
-class App extends Component{
+const App=()=>{
 
-  state={
+   const [personState,setPersonState] = useState({
     persons:[
       {name:'max',age:23},
       {name:'Arun1',age:34}
-    ],
-    newPerson:'Gooder'
-  };
-
-  nameHandler = () =>{
-    this.setState({
+    ]
+   });
+   const [newPersonState,setNewPerson] = useState({
+      newPerson:'Gooder'
+   });
+  var nameHandler = () =>{
+    setPersonState({
       persons:[
-        {name:'max!!',age:23},
+        {name:'maxiiiiiiiiiiiiiiiiiiiii',age:23},
         {name:'Arun1',age:34}
-      ],
-      newPerson:'GoodMillain'
-    }); 
-    console.log(this.state.newPerson);/*this is checking if the previous state is also maintained --> yes it is */
+      ]
+    });
+    setNewPerson({
+      newPerson:'maxgoodperson'
+    });
   }
 
-  
-   render(){
      return(
        <div className="App">
          <p>this is a para test from a class component of react</p>
          <h1>this is the heading component</h1>
-         <button onClick={this.nameHandler}>Click here</button> 
-         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-         <Person name={this.state.newPerson} age="24"/> 
+         <button onClick={nameHandler}>Click here</button> 
+         <Person name={personState.persons[0].name} age={personState.persons[0].age}/>
+         <Person name={personState.persons[1].name} age={personState.persons[1].age}/>
+         <Person name={newPersonState.newPerson} age="24"/> 
        </div>
      );
-   }
+
 }
 
 export default App;
